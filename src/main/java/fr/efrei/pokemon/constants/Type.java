a2062f80a338d98dbc35450dd61fe59f2720962e
En nouary <ayoub.ennouary03@gmail.com>
2024-10-22 12:26:36 +0200

    Ajout de nouveaux fichiers

---
+++ src/main/java/fr/efrei/pokemon/constants/Type.java
@@ -0,0 +1,11 @@
+package fr.efrei.pokemon.constants;
+
+public enum Type {
+  PSY,
+  ELECTRIC,
+  FEU,
+  EAU,
+  PLANTE,
+  INSECTE,
+  NORMAL
+}
---
+++ src/main/java/fr/efrei/pokemon/repositories/TrainerRepository.java
@@ -0,0 +1,10 @@
+package fr.efrei.pokemon.repositories;
+
+import fr.efrei.pokemon.model.Trainer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TrainerRepository extends JpaRepository<Trainer, String >{
+
+}
